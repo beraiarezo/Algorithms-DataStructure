393b7aa3806f93b7d2cec88827dec5e2530feeaa
rezoberaia <beraiarezo@gmail.com>
2024-04-07 02:17:34 +0400

    Correct bin pow implementation

--- Algebra/bin-pow.h
+++ Algebra/bin-pow.h
@@ -1,8 +1,21 @@
 #ifndef BINARYEXP_H
 #define BINARYEXP_H
 
-long long recursivePow(long long a, long long b);
+long long recursiveBinPow(long long a, long long b) {
+  if(b == 0) return 1;
+  long long result = recursiveBinPow(a, b/2);
+  if(b % 2) return result * result * a;
+  else return result * result;
+}
 
-long long linearPow(long long a, long long b);
+long long linearBinPow(long long a, long long b) {
+  long long result = 1;
+  while(b > 0) {
+    if(b & 1) result = result * a;
+    a *= a;
+    b >>= 1;
+  }
+  return result;
+}
 
 #endif
